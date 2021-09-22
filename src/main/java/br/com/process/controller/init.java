32c9850f3e0db19d4bd71eb81712f0524413c681
Icaro Sousa <icarosousa131@gmail.com>
2021-09-22 20:39:11 -0300

    lista e Excluir

--- src/main/java/br/com/process/controller/init.java
+++ src/main/java/br/com/process/controller/init.java
@@ -1,6 +1,8 @@
 package br.com.process.controller;
 
 import br.com.process.DAO.ProdutoDAO;
+import br.com.process.entidade.Produto;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +37,49 @@ public class init {
         return "cadastro";
     }
     
+    @RequestMapping("add")
+    public String add(Model model, Produto produto){
+        try {
+            if(ProdutoDAO.Adicionar(produto)){
+                model.addAttribute("MSG", "Adicionado com Sucesso");
+                return "mensagem";
+            } else{
+                model.addAttribute("MSG", "Erro ao Adicionar");
+                return "mensagem";
+            }
+        } catch (Exception e) {
+            model.addAttribute("MSG", e);
+            return "mensagem";
+        }        
+    }
+    
+    @RequestMapping("/adim/listaProduto")
+    public String listaProduto (Model model){
+        model.addAttribute("listaProduto", ProdutoDAO.getEstoque());
+        return "listaProduto";
+    }
+    
+    @RequestMapping("Excluir")
+    public String Excluir(Model model, HttpServletRequest request){
+        try {
+            Produto produto = new Produto(Integer.parseInt(request.getParameter("ID")));
+            if(ProdutoDAO.Excluir(produto)){
+                model.addAttribute("MSG", "Adicionado com Sucesso");
+            } else{
+                model.addAttribute("MSG", "Erro ao Adicionar");
+            }
+        } catch (Exception e) {
+            model.addAttribute("MSG", e);
+        }
+        return "mensagem";
+    }
+    
+    @RequestMapping("seila")
+    public String seila(Model model){
+        model.addAttribute("MSG", "Sei la");
+        return "mensagem";
+    }
+
     @RequestMapping("/")
     public String index(Model model){
         try {
